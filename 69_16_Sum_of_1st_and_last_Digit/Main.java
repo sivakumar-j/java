a8d1196bada0e220cb46371a2942b08b025b2adc
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-08-10 21:52:59 +0530

    Create Main.java

---
+++ 69_16_Sum_of_1st_and_last_Digit/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Sum of 1st and Last Dig is "+
+                FirstLastDigitSum.sumFirstAndLastDigit(1024));
+    }
+}
