74dd4b11d93f9bba0bf8ef6c3f80f5c00b5415df
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-08-10 22:06:41 +0530

    Create Main.java

---
+++ 69_20_GreatestCommonDivisor/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("GCM is "+ GreatestCommonDivisor.getGreatestCommonDivisor(81,153));
+    }
+}
