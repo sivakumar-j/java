bc03636a27a345314193896c648698e3d22dbde2
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-08-31 09:51:13 +0530

    Update GroceryList.java

--- basics/26_arrays_example/example_3/GroceryList.java
+++ basics/26_arrays_example/example_3/GroceryList.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.util.ArrayList;
 
-
 public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<String>();
 
@@ -24,19 +23,20 @@ public class GroceryList {
     }
 
     public void modifyGroceryItem(String currentItem, String newItem) {
-        int position = findItem(currentItem);
-        if(position >= 0) {
-            modifyGroceryItem(position, newItem);
+        int currentItem_position = findItem(currentItem);
+        if(currentItem_position >= 0) {
+            modifyGroceryItem(currentItem_position, newItem);
         }
 
     }
+
     private void modifyGroceryItem(int position, String newItem) {
         groceryList.set(position, newItem);
         System.out.println("Grocery item " + (position+1) + " has been modified.");
     }
 
-    public void removeGroceryItem(String item) {
-        int position = findItem(item);
+    public void removeGroceryItem(String itemName) {
+        int position = findItem(itemName);
         if(position >= 0) {
             removeGroceryItem(position);
         }
@@ -51,6 +51,7 @@ public class GroceryList {
 
     public boolean onFile(String searchItem) {
         int position = findItem(searchItem);
+
         if(position >=0) {
             return true;
         }
