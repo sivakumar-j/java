64e85e39cdc185a6dca5e5c8ac7edca4d6b71b44
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-09-09 19:14:32 +0530

    Update Printer.java

--- basics/33_abstract/Printer.java
+++ basics/33_abstract/Printer.java
@@ -6,6 +6,7 @@ class Printer
 		System.out.println(i);
 	}
 //	public void show(Double i) //this method is not needed since Show can be called with int,float or double since it is a Number
+				   // this is possible because Number is a abstract class and Interger,Double extends from Number	
 //	{
 //		System.out.println(i);
 //	}
