a4463860e5e9d234cc8c40ee5f2bac1c9cfc7011
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-10-10 11:26:04 +0530

    Create n11_Generics_Lowerbound.java

---
+++ basics/35_collections/generics/n11_Generics_Lowerbound.java
@@ -0,0 +1,46 @@
+package com.basics.packageone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n11_Generics_Lowerbound {
+
+	public static void addCat(List<? super Cat> catList) {
+		catList.add(new BlackCat());
+		System.out.println("Cat Added");
+	}
+
+	public static void main(String[] args) {
+		List<Animal> animalList = new ArrayList<Animal>();
+		List<Cat> catList = new ArrayList<Cat>();
+		List<BlackCat> blackCatList = new ArrayList<BlackCat>();
+		List<Dog> dogList = new ArrayList<Dog>();
+
+		// add list of super class Animal of Cat class
+		addCat(animalList);
+
+		// add list of Cat class
+		addCat(catList);
+
+		// compile time error
+		// can not add list of subclass RedCat of Cat class
+		// addCat(blackCatList);
+
+		// compile time error
+		// can not add list of subclass Dog of Superclass Animal of Cat class
+		// addCat.addMethod(dogList);
+	}
+
+}
+
+class Animal {
+}
+
+class Cat extends Animal {
+}
+
+class BlackCat extends Cat {
+}
+
+class Dog extends Animal {
+}
