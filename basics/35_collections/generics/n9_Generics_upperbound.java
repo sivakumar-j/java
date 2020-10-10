be7bbca305c595b798be50e2e0b46ac4f9e9d8b9
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-10-10 11:25:17 +0530

    Create n9_Generics_upperbound.java

---
+++ basics/35_collections/generics/n9_Generics_upperbound.java
@@ -0,0 +1,24 @@
+package com.basics.packageone;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class n9_Generics_upperbound {
+
+	public static double sum(List<? extends Number> numberlist) {
+		double sum = 0.0;
+		for (Number n : numberlist)
+			sum += n.doubleValue();
+		return sum;
+	}
+
+	public static void main(String args[]) {
+		List<Integer> integerList = Arrays.asList(1, 2, 3);
+		System.out.println("sum = " + sum(integerList));
+
+		List<Double> doubleList = Arrays.asList(1.2, 2.3, 3.5);
+		System.out.println("sum = " + sum(doubleList));
+	}
+
+}
+
