d4620d1405a010482bd8e62b35a726bcc131d8b9
Sivakumar <44526234+sivakumar-j@users.noreply.github.com>
2020-11-22 08:56:39 +0530

    Update n7_CustomFunctionalInterface.java

--- level_2/functional_programming/n7_CustomFunctionalInterface.java
+++ level_2/functional_programming/n7_CustomFunctionalInterface.java
@@ -1,11 +1,15 @@
 package com.leveltwo.packageone;
 
+import java.util.Arrays;
 //program to give demo that what goes behing in Functional programming
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 class EvenNumberPredicate implements Predicate<Integer> {
+	// Predicate accepts i/p and returns a boolean to stream
+
 	@Override
 	public boolean test(Integer num) {
 		return num % 2 == 0;
@@ -13,10 +17,26 @@ class EvenNumberPredicate implements Predicate<Integer> {
 }
 
 class SysOutConsumer implements Consumer<Integer> {
+	// Consumer accepts i/p and returns nothing to stream
 	@Override
 	public void accept(Integer num) {
 		System.out.println(num);
 	}
+}
+
+class SupplierDemo implements Supplier<String>{
+	// Supplier accepts nothing and returns anything;
+
+	// List<String> list1 = Arrays.asList("a");
+
+	@Override
+	public String get() {
+		// TODO Auto-generated method stub
+		return "Nothing found";
+	}
+
+	// System.out.println(list1.stream().findAny().orElseGet(() -> "Hi viewers"));
+
 }
 
 public class n7_CustomFunctionalInterface {
@@ -33,6 +53,10 @@ public class n7_CustomFunctionalInterface {
 		numbers.stream()
 		.filter(new EvenNumberPredicate())
 		.forEach(new SysOutConsumer());
+
+		// List<String> list1 = Arrays.asList("apple", "bat");
+		List<String> list1 = Arrays.asList();
+		System.out.println(list1.stream().findAny().orElseGet(new SupplierDemo()));
 	}
 }
 
